138949fff82b46a61d6144382f28b7a30796dcc8
dxiong7 <danielxiong523@gmail.com>
2019-01-30 15:27:18 -0800

    Initial Commit

---
+++ tip-calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip-calc
+//
+//  Created by Daniel Xiong on 1/30/19.
+//  Copyright © 2019 Daniel Xiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
