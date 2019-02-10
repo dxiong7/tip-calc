86ad79595e6e7fdf9d4d0d4e7f9a19afe40cf148
dxiong7 <danielxiong523@gmail.com>
2019-02-10 00:44:00 -0800

    bill amount will persist for 10 minutes across app restarts

--- tip-calc/SettingsViewController.swift
+++ tip-calc/SettingsViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class SettingsViewController: UIViewController {
     
     @IBOutlet weak var defaultTipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,22 +21,23 @@ class SettingsViewController: UIViewController {
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true);
     }
+    override func viewWillAppear(_ animated: Bool) {
+        let defaults = UserDefaults.standard
+        defaultTipControl.selectedSegmentIndex = defaults.integer(forKey: "myDefaultTipIndex")
+    }
     override func viewWillDisappear(_ animated: Bool) {
         
         super.viewWillDisappear(animated)
-        print("view will disappear")
+        //print("view will disappear")
         let defaults = UserDefaults.standard
         let tipPercentages = [0.1, 0.15, 0.2]
         let defaultTipIndex = defaultTipControl.selectedSegmentIndex
         
-        print("defaultTipIndex is ", defaultTipIndex)
+        //print("defaultTipIndex is ", defaultTipIndex)
         let defaultTip = tipPercentages[defaultTipIndex]
         defaults.set(defaultTip, forKey: "myDefaultTipPercentage")
         defaults.set(defaultTipIndex, forKey: "myDefaultTipIndex")
         defaults.synchronize()
-        
-    }
-    @IBAction func changeTip(_ sender: Any) {
         
     }
     /*
--- tip-calc/ViewController.swift
+++ tip-calc/ViewController.swift
@@ -18,22 +18,45 @@ class ViewController: UIViewController {
     var customTipPercentage = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.title = "Tip Calculator"
+        
+        let defaults = UserDefaults.standard
+        
+        let lastTimeDiff = Date(timeIntervalSinceNow: TimeInterval(10))
+        if (Date() < lastTimeDiff) {
+            print("remembering last bill amount and setting")
+            if (defaults.double(forKey: "BillAmount") == 0) {
+                billField.text = ""
+            }
+            else {
+                billField.text = String(defaults.double(forKey: "BillAmount"))
+            }
+            let bill = Double(billField.text!) ?? 0
+            let tipPercentages = [0.1, 0.15, 0.2]
+            let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+            let total = bill + tip
+            
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+            
+            //save bill amount in user Defaults
+            defaults.set(bill, forKey: "BillAmount")
+            defaults.synchronize()
+            
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("view will appear")
-        //tipControl.selectedSegmentIndex = 3
+        let defaults = UserDefaults.standard
         let bill = Double(billField.text!) ?? 0
         
-        let defaults = UserDefaults.standard
         let tip = defaults.double(forKey: "myDefaultTipPercentage") * bill
         tipControl.selectedSegmentIndex = defaults.integer(forKey: "myDefaultTipIndex")
         let total = bill + tip
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        billField.becomeFirstResponder()
         // This is a good place to retrieve the default tip percentage from UserDefaults
         // and use it to update the tip amount
     }
@@ -47,6 +70,8 @@ class ViewController: UIViewController {
     @IBAction func calculateTip(_ sender: Any) {
         //gets the bill amount, calculates tip and total
         //and updates the tip and total labels
+        let defaults = UserDefaults.standard
+        
         let bill = Double(billField.text!) ?? 0
         let tipPercentages = [0.1, 0.15, 0.2]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
@@ -54,6 +79,10 @@ class ViewController: UIViewController {
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        
+        //save bill amount in user Defaults
+        defaults.set(bill, forKey: "BillAmount")
+        defaults.synchronize()
         
         
     }
@@ -65,12 +94,19 @@ class ViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        print("view will disappear")
+        
+        let defaults = UserDefaults.standard
+        
+        let bill = Double(billField.text!) ?? 0
+        defaults.set(bill, forKey: "BillAmount")
+        defaults.synchronize()
+        
+        //print("view will disappear")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("view did disappear")
+        //print("view did disappear")
     }
 }
 
